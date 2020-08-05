3cf1639cf8c764fa38128b6a32cf2fce0fed9e35
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-08-04 21:09:45 -0400

    Docs, db creation, and most of data population scripts complete

--- Assessment - Hotel Reservation Schema/NarishSingh-HotelDB.sql
+++ Assessment - Hotel Reservation Schema/NarishSingh-HotelDB.sql
@@ -3,27 +3,24 @@ CREATE DATABASE Hotel;
 USE Hotel;
 
 --  room children
-CREATE TABLE RoomType(
-	TypeId INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE RoomType (
+    RoomTypeId INT PRIMARY KEY AUTO_INCREMENT,
     TypeName VARCHAR(6) NOT NULL,
-    BasePrice DECIMAL(5,2) NOT NULL,
-    ExtraPersonFee DECIMAL(4,2) NOT NULL,
+    BasePrice DECIMAL(5, 2) NOT NULL,
+    ExtraPersonFee DECIMAL(4, 2) NOT NULL,
     StandardOccupany INT,
     MaxOccupancy INT NOT NULL
 );
 
-CREATE TABLE RoomAmenity(
+CREATE TABLE Amenity (
 	AmenityId INT PRIMARY KEY AUTO_INCREMENT,
-	AdaAccessible BOOLEAN NOT NULL,
-    HasMicrowave BOOLEAN NOT NULL,
-    HasJacuzzi BOOLEAN NOT NULL,
-    HasOven BOOLEAN NOT NULL,
-    HasRefrigerator BOOLEAN NOT NULL
+    AmenityName VARCHAR(15) NOT NULL,
+    AmenityCost DECIMAL(4, 2) NOT NULL
 );
 
 -- guest child
-CREATE TABLE GuestAddress(
-	AddressId INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE GuestAddress (
+    GuestAddressId INT PRIMARY KEY,
     Address VARCHAR(50) NOT NULL,
     City VARCHAR(50) NOT NULL,
     State CHAR(2) NOT NULL,
@@ -31,37 +28,46 @@ CREATE TABLE GuestAddress(
 );
 
 -- parent tables 
-CREATE TABLE Room(
-	RoomNum INT PRIMARY KEY,
-    TypeId INT NOT NULL,
+CREATE TABLE Room (
+    RoomNum INT PRIMARY KEY,
+    RoomTypeId INT NOT NULL,
+    AdaAccessible BOOLEAN NOT NULL,
+    CONSTRAINT `fk_Room_RoomType` FOREIGN KEY (RoomTypeId)
+        REFERENCES RoomType (RoomTypeId)
+); 
+
+-- -- RoomAmenity bridge table
+CREATE TABLE RoomAmenity (
+    RoomNum INT NOT NULL,
     AmenityId INT NOT NULL,
-    CONSTRAINT `fk_Room_RoomType` FOREIGN KEY (TypeId)
-		REFERENCES RoomType(TypeId),
-	CONSTRAINT `fk_Room_RoomAmenity` FOREIGN KEY (AmenityId)
-		REFERENCES RoomAmenity(AmenityId)
+    PRIMARY KEY (RoomNum, AmenityId),
+    CONSTRAINT `fk_RoomAmenity_AmenityId` FOREIGN KEY (AmenityId)
+		REFERENCES Amenity (AmenityId),
+	CONSTRAINT `fk_RoomAmenity_RoomNum` FOREIGN KEY (RoomNum)
+		REFERENCES Room (RoomNum)
 );
 
-CREATE TABLE Guest(
-	NameId INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Guest (
+    GuestID INT PRIMARY KEY AUTO_INCREMENT,
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
     PhoneNumber CHAR(12) NOT NULL,
-    AddressId INT NOT NULL,
-    CONSTRAINT `fk_Room_Guest` FOREIGN KEY (AddressId)
-		REFERENCES GuestAddress(AddressId)
+    GuestAddressId INT NOT NULL,
+    CONSTRAINT `fk_Guest_GuestAddress` FOREIGN KEY (GuestAddressId)
+        REFERENCES GuestAddress (GuestAddressId)
 );
 
-CREATE TABLE Reservation(
-	ReservationId INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Reservation (
+    ReservationId INT PRIMARY KEY AUTO_INCREMENT,
     StartDate DATE NOT NULL,
     EndDate DATE NOT NULL,
     AdultCount INT NOT NULL,
     ChildCount INT NOT NULL,
-    TotalRoomCost DECIMAL(6,2) NOT NULL,
-    NameId INT NOT NULL,
+    TotalRoomCost DECIMAL(6, 2) NOT NULL,
+    GuestID INT NOT NULL,
     RoomNum INT NOT NULL,
-    CONSTRAINT `fk_Reservation_Guest` FOREIGN KEY (NameId)
-		REFERENCES Guest(NameId),
-	CONSTRAINT `fk_Reservation_Room` FOREIGN KEY(RoomNum)
-		REFERENCES Room(RoomNum)
-);
+    CONSTRAINT `fk_Reservation_Guest` FOREIGN KEY (GuestID)
+        REFERENCES Guest (GuestID),
+    CONSTRAINT `fk_Reservation_Room` FOREIGN KEY (RoomNum)
+        REFERENCES Room (RoomNum)
+); 
