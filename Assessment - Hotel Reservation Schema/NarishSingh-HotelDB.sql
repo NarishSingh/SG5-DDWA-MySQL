84556c28bf49438662932a4372e80d0c87348d5a
Narish Singh <narish.singh786@gmail.com>
2022-06-03 14:54:11 -0400

    formatting

--- Assessment - Hotel Reservation Schema/NarishSingh-HotelDB.sql
+++ Assessment - Hotel Reservation Schema/NarishSingh-HotelDB.sql
@@ -13,7 +13,7 @@ CREATE TABLE RoomType (
 );
 
 CREATE TABLE Amenity (
-	AmenityId INT PRIMARY KEY AUTO_INCREMENT,
+    AmenityId INT PRIMARY KEY AUTO_INCREMENT,
     AmenityName VARCHAR(15) NOT NULL,
     AmenityCost DECIMAL(4, 2) NOT NULL
 );
@@ -32,6 +32,7 @@ CREATE TABLE Room (
     RoomNum INT PRIMARY KEY,
     RoomTypeId INT NOT NULL,
     AdaAccessible BOOLEAN NOT NULL,
+	
     CONSTRAINT `fk_Room_RoomType` FOREIGN KEY (RoomTypeId)
         REFERENCES RoomType (RoomTypeId)
 ); 
@@ -41,10 +42,11 @@ CREATE TABLE RoomAmenity (
     RoomNum INT NOT NULL,
     AmenityId INT NOT NULL,
     PRIMARY KEY (RoomNum, AmenityId),
+	
     CONSTRAINT `fk_RoomAmenity_AmenityId` FOREIGN KEY (AmenityId)
-		REFERENCES Amenity (AmenityId),
-	CONSTRAINT `fk_RoomAmenity_RoomNum` FOREIGN KEY (RoomNum)
-		REFERENCES Room (RoomNum)
+	REFERENCES Amenity (AmenityId),
+    CONSTRAINT `fk_RoomAmenity_RoomNum` FOREIGN KEY (RoomNum)
+	REFERENCES Room (RoomNum)
 );
 
 CREATE TABLE Guest (
@@ -53,6 +55,7 @@ CREATE TABLE Guest (
     LastName VARCHAR(50) NOT NULL,
     PhoneNumber CHAR(12) NOT NULL,
     GuestAddressId INT NOT NULL,
+	
     CONSTRAINT `fk_Guest_GuestAddress` FOREIGN KEY (GuestAddressId)
         REFERENCES GuestAddress (GuestAddressId)
 );
@@ -66,6 +69,7 @@ CREATE TABLE Reservation (
     TotalRoomCost DECIMAL(6, 2) NOT NULL,
     GuestID INT NOT NULL,
     RoomNum INT NOT NULL,
+	
     CONSTRAINT `fk_Reservation_Guest` FOREIGN KEY (GuestID)
         REFERENCES Guest (GuestID),
     CONSTRAINT `fk_Reservation_Room` FOREIGN KEY (RoomNum)
